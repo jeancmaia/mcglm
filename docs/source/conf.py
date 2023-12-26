f1ae5af63f73841ce2ef1d02829ade6784549606
jean <jean@pop-os.localdomain>
2023-12-26 19:38:15 -0300

    changing sys

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../mcglm/'))
+sys.path.insert(0, os.path.abspath('mcglm'))
 
 project = 'mcglm'
 copyright = '2023, Jean Carlos Faoot Maia'
